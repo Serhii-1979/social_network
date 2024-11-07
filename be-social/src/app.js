afa4fd770d28d2aeed6a0cb0d0e354fe18fe7cbb
Serhii-1979 <krasovskyi76@gmail.com>
2024-11-07 01:44:10 +0100

    Rebace Cloudinary

--- be-social/src/app.js
+++ be-social/src/app.js
@@ -1,15 +1,24 @@
-import express from 'express';
-import cors from 'cors';
-import { v2 as cloudinary } from 'cloudinary';
-import authRoutes from './routes/authRoutes.js';
-import userRoutes from './routes/userRoutes.js';
-import postRoutes from './routes/postRoutes.js';
-import commentRoutes from './routes/commentRoutes.js';
-import searchRoutes from './routes/searchRoutes.js';
-import likeRoutes from './routes/likeRoutes.js';
-import followRoutes from './routes/followRoutes.js';
-import notificationRoutes from './routes/notificationRoutes.js';
+import express from "express";
+import cors from "cors";
+import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import postRoutes from "./routes/postRoutes.js";
+import commentRoutes from "./routes/commentRoutes.js";
+import searchRoutes from "./routes/searchRoutes.js";
+import likeRoutes from "./routes/likeRoutes.js";
+import followRoutes from "./routes/followRoutes.js";
+import notificationRoutes from "./routes/notificationRoutes.js";
 
+dotenv.config();
+
+// Настройка Cloudinary с использованием переменных из .env
+export const cloud = cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 // Инициализация приложения Express
 const app = express();
@@ -21,14 +30,14 @@ app.use(cors());
 app.use(express.json());
 
 // Подключение маршрутов
-app.use('/api/auth', authRoutes);
-app.use('/api/user', userRoutes);
-app.use('/api/post', postRoutes);
-app.use('/api/comments', commentRoutes);
-app.use('/api/search', searchRoutes);
-app.use('/api/likes', likeRoutes);
-app.use('/api/follow', followRoutes);
-app.use('/api/notifications', notificationRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/post", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/search", searchRoutes);
+app.use("/api/likes", likeRoutes);
+app.use("/api/follow", followRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 // Экспорт приложения для использования в server.js
 export default app;
